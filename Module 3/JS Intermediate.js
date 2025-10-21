5e9e4464a3cfbd09d347e21a845026427e1b1157
Sebastian Gordon <sebgordon99@gmail.com>
2025-10-21 16:46:57 +1100

    js intermediate start

---
+++ Module 3/JS Intermediate.js
@@ -0,0 +1,53 @@
+// Question 1:
+
+function ucFirstLetters(string) {
+    const arrayOfStrings = string.split(" ");
+    let emptyArray = []
+    for (let i of arrayOfStrings) {
+       i = i[0].toUpperCase() + i.slice(1, 11);
+       emptyArray.push(i)
+    //    console.log(i)
+    }
+    
+    return emptyArray.join(" ");
+}
+console.log(ucFirstLetters("los angeles") ) // Los Angeles
+console.log(ucFirstLetters("lowercase word") ) // Lowercase Word
+console.log(ucFirstLetters("I could be anything you like") ) // 
+
+
+// Question 2:
+
+
+
+// Question 3:
+
+
+
+// Question 4:
+
+
+
+// Question 5:
+
+
+
+// Question 6:
+
+
+
+// Question 7:
+
+
+
+// Question 8:
+
+
+
+// Question 9:
+
+
+
+// Question 10:
+
+
