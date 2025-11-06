1287fd6a57dea5a6ee6cf77dcc58c605b88a7245
Sebastian Gordon <sebgordon99@gmail.com>
2025-11-06 15:12:26 +1100

    module 5 labs

---
+++ Module 5/myapp/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const app2 = express();
+const port = 3003;
+const port2 = 3004;
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at
+http://localhost:${port}`);
+});
+
+app2.get("/", (req, res) => {
+  res.send("Hello World 2!");
+});
+
+app2.listen(port2, () => {
+  console.log(`Example app listening at
+http://localhost:${port2}`);
+});
+
+app.use("/", express.static("public"));
