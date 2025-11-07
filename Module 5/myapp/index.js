b4cd4be62f31a5e891757f70a8675418dbd1903b
Sebastian Gordon <sebgordon99@gmail.com>
2025-11-07 14:31:08 +1100

    cors bs

--- Module 5/myapp/index.js
+++ Module 5/myapp/index.js
@@ -7,7 +7,7 @@ const testRoutes =
 require('./routes/myTestRoutes');
 
 const cors = require("cors");
-const corsOptions = {origin: "http://localhost:5501"};
+const corsOptions = {origin: "http://127.0.0.1:5501"};
 app.use(cors(corsOptions));
 
 // import all calculator routes (up the top)
