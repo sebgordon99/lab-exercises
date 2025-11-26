458ea3e2b776895999e3ebc09a94b68c64a1aee7
Sebastian Gordon <sebgordon99@gmail.com>
2025-11-27 09:58:37 +1100

    module 9 lab 3

--- Module 9/mongodb-app-lab2/controllers/postController.js
+++ Module 9/mongodb-app-lab2/controllers/postController.js
@@ -41,6 +41,16 @@ const deleteUserPost = (req, res) => {
     });
 };
 
+const createAPIUserPost = (req, res) => {
+  new Models.Post({ ...req.body, author: req.params.id })
+    .save()
+    .then((data) => res.send({ result: 200, data: data }))
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
 module.exports = {
   getUserPosts,
   createUserPost,
